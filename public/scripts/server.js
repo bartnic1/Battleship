9a815ace5ff9b97c561037b790d54a60d5a2feb6
Piotr <bartnic1@gmail.com>
2018-02-27 06:37:14 +0000

    Finished basic battle log div element, and added a notifications panel to provide a basic UI for the game. Most of the work took place in the battle.css file

--- public/scripts/server.js
+++ public/scripts/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,"../../public")));
 
 const gridSettings = {
-  gridBoundary: {letters: 'abcdefghij'}
+  gridBoundary: {letters: 'ABCDEFGHIJ'}
 };
 
 app.get("/battle", (req, res) => {
